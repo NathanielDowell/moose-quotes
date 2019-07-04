e4c46938d5aba552800a24d6bde7b0e22913e633
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-07-04 14:03:24 -0500

    folder structure and example properties updated

---
+++ src/main/java/com/example/moosequotesgenerator/MooseQuotesGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.moosequotesgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooseQuotesGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooseQuotesGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moosequotesgenerator/MooseQuotesGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.moosequotesgenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MooseQuotesGeneratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
